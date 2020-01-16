9be5977d201c8f9b7ff3722e3757cf0b3d81878b
Eduardo Peixe <>
2020-01-16 13:47:05 -0500

    Add router gorilla mux

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	
+	"github.com/gorilla/mux"
 )
 
 const port = ":3000"
@@ -11,17 +13,28 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	switch r.URL.Path {
 	case "/":
-		fmt.Fprint(w, "<h1>Welcome to instaGO!</h1>")
 	case "/contact":
-		fmt.Fprint(w, "<p>Please send an email to <a href=#>support@instago.com</a></p>")
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>404 Page not found</h1>")
 	}
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to instaGO!</h1>")
+	
+}
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>Please send an email to <a href=#>support@instago.com</a></p>")
+
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	fmt.Println("Serving port", port)
-	http.ListenAndServe(port, nil)
+	http.ListenAndServe(port, r)
 }
