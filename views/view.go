eabf7475db8726baf635d45a292496bfa9f98aaa
Eduardo Peixe <>
2020-01-16 18:53:19 -0500

    Create view type

---
+++ views/view.go
@@ -0,0 +1,20 @@
+package views
+
+import (
+	"html/template"
+)
+
+// NewView creates a view
+func NewView(files ...string) *View {
+	files = append(files, "viewa/layouts/footer.gohtml")
+
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//View is the type of a view
+type View struct {
+	Template *template.Template
+}
