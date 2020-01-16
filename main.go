43566053bc66979f9f7e0f7eefc9ae23a697b68f
Eduardo Peixe <>
2020-01-16 14:19:56 -0500

    Add faq and custom 404 pages

--- main.go
+++ main.go
@@ -15,8 +15,6 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	case "/":
 	case "/contact":
 	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404 Page not found</h1>")
 	}
 }
 
@@ -25,16 +23,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to instaGO!</h1>")
 	
 }
+
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<p>Please send an email to <a href=#>support@instago.com</a></p>")
-
+	
 }
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "this is the FAQ page")
+	
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>404 Page not found</h1>")
+}
+
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	fmt.Println("Serving port", port)
 	http.ListenAndServe(port, r)
 }
