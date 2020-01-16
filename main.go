58205a31bfc2be0d0dd46a205b4903923f37fa0e
Eduardo Peixe <>
2020-01-16 13:12:18 -0500

    Change output to contact page

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"net/http"
 )
 
+const port = ":3000"
+
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to instaGO!</h1>")
+	fmt.Fprint(w, "<p>Please send an email to <a href=#>support@instago.com</a></p>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Serving port", port)
+	http.ListenAndServe(port, nil)
 }
