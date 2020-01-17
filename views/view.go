81218e08ee482fd561ca01bd8adf759a486b2327
Eduardo Peixe <>
2020-01-17 06:23:54 -0500

    Clean up view code

--- views/view.go
+++ views/view.go
@@ -2,15 +2,17 @@ package views
 
 import (
 	"html/template"
+	"path/filepath"
+)
+
+const (
+	layoutDir = "views/layouts/"
+	templateExt = ".gohtml" 
 )
 
 // NewView creates a view
 func NewView(layout string, files ...string) *View {
-	files = append(files, 
-		"views/layouts/bootstrap.gohtml", 
-		"views/layouts/footer.gohtml",
-		"views/layouts/navbar.gohtml",
-	)
+	files = append(files, layoutfiles()...) 
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -24,4 +26,14 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layoutFile return a slice of strings representing the layout files
+func layouts()  []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+
+	return files
 }
