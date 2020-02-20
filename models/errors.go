de62fe3e7f7b851750c87b66ef273cdbd95f32d1
Eduardo Peixe <>
2020-02-20 07:18:01 -0500

    Implemented a PublicError interface

---
+++ models/errors.go
@@ -0,0 +1,39 @@
+package models
+
+import "strings"
+
+var (
+	// ErrNotFound is a generic error for resource not found
+	ErrNotFound modelError = "models: resource not found"
+	// ErrInvalidID when an invalid ID is provided
+	ErrInvalidID modelError = "models: invalid ID"
+	// ErrInvalidEmail when an invalid email is provided
+	ErrInvalidEmail modelError = "models: invalid email address"
+	// ErrIncorrectPassword when a invalid password is provided
+	ErrIncorrectPassword modelError = "models: incorrect password provided"
+	//ErrEmailRequired when an email is not present
+	ErrEmailRequired modelError = "models: email address is required"
+	//ErrEmailInvalid when email is not in the correct format
+	ErrEmailInvalid modelError = "models: email address is invalid"
+	//ErrEmailTaken when a user with same email already exists
+	ErrEmailTaken modelError = "models: email address is already taken"
+	//ErrPasswordTooShort when a password's length is not enough
+	ErrPasswordTooShort modelError = "models: password must be at least 8 characters long"
+	//ErrPasswordRequired when a password is not present
+	ErrPasswordRequired modelError = "models: password is required"
+	//ErrPasswordHashRequired when a password is not present
+	ErrPasswordHashRequired modelError = "models: password hash not present"
+)
+
+type modelError string
+
+func (e modelError) Error() string {
+	return string(e)
+}
+
+func (e modelError) Public() string {
+	s := strings.Replace(string(e), "models: ", "", 1)
+	split := strings.Split(s, " ")
+	split[0] = strings.Title(split[0])
+	return strings.Join(split, " ")
+}
